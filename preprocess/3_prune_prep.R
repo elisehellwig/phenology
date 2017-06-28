d35d2a5ed7868cdb4bb6abd6af6c36a3e71752df
elisehellwig <echellwig@ucdavis.edu>
2017-06-28 09:37:00 -0500

    cleaning up praw1

--- preprocess/3_prune_prep.R
+++ preprocess/3_prune_prep.R
@@ -15,13 +15,19 @@ options(stringsAsFactors = FALSE)
 cl <- read.csv(file.path(drivepath,'data/clean/croploc.csv'))
 
 ########################Prunes###################################
-praw1 <- read.csv(file.path(importpath,'french1988.csv'))
+praw1 <- read.csv(file.path(importpath,'french1988.csv'))[,c(1:5,10)]
 praw2 <- read.csv(file.path(importpath,'NSVPrune.csv'))
 
-####################Praw2#############################
+####################Praw1#############################
+names(praw1) <- c('bstart','bend','maturity','cultivar','loc','year')
 
-
-
+p1melt <- melt(praw1, id.vars=c('cultivar','loc','year'),
+               measure.vars = c('bstart','bend','maturity'),
+               variable.name = 'event',
+               value.name = 'date')
+                
+p1melt$Date <- as.Date(p1melt$date, format="%m/%d/%y")
+p1melt$day <- yday(p1melt$Date)
 
 
 ####################Praw2#############################
