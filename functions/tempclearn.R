d0ad4dc4bc431fd8b923e467465861a6ad9c0194
elisehellwig <echellwig@ucdavis.edu>
2017-07-02 14:11:51 -0500

    so I wrote to functions to download and reformat daily temp data from the NCDC

--- functions/tempclearn.R
+++ functions/tempclearn.R
@@ -1,3 +1,67 @@
+
+ghcnd_reshape <- function(df, vars=c('TMAX','TMIN'), valuename='VALUE') {
+    
+    valcols <- grep('VALUE', names(df))
+    
+    dfr <- cbind(df[,c('id', 'year','month','element')], df[,valcols]) 
+    
+    dfr <- dfr[dfr$element %in% vars, ]
+    
+    dfmelt <- melt(dfr, id.vars = c('id', 'year','month','element'), 
+                   variable.name = 'day', value.name = 'temp')
+    dfmelt$day <- as.character(dfmelt$day)
+    
+    dfmelt$day <- gsub(valuename, '', dfmelt$day)
+    dfmelt$day <- as.integer(dfmelt$day)
+    
+    
+    dfcast <- dcast(dfmelt, id + year + month + day ~ element,
+                    value.var = 'temp')
+    
+    names(dfcast)[5:6] <- c('tmin','tmax')
+    
+    return(dfcast)
+    
+}
+
+
+ghncd_download <- function(stations, 
+                           APItoken='LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN') {
+    require(plyr)
+    require(lubridate)
+    
+    stnids <- paste0('GHCND:', stations)
+    
+    meta <- ldply(stnids, function(id) {
+        voi <- c('longitude','latitude','elevation')
+        ncdc_stations(stationid=id, token=APItoken)$data[,voi]
+        })
+    
+    meta$id <- stations
+    
+    
+    dailyT <- lapply(stations, function(id) {
+        ghcnd(stationid=id, token=APItoken)
+    })
+    
+    dailyt <- ldply(dailyT, function(dt) {
+        ghcnd_reshape(dt)
+    })
+    
+    
+    dt <- merge(dailyt, meta)
+    
+    dt$date <- as.Date(paste(dt$year, dt$month, dt$day, sep='-'))
+    dt$jd <- yday(dt$date)
+    
+}
+
+
+
+
+
+
+
 tempclean <- function(years, data, primary, secondary, hourly=FALSE,
                       dateform ='%Y%m%d', na_val=-9999) {
     #columns should be in the order location, date, temp/tmin, (tmax)
@@ -40,7 +104,9 @@ tempclean <- function(years, data, primary, secondary, hourly=FALSE,
         })
     }
     
+    primaryNAs <- union(which(is.na(pri$tmin)), which(is.na(pri$tmax)))
     
+    secondaryNAs <- lapply()
     
     
     
