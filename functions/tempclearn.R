f5d5346922f4caac9153f0035a1759b2a6fa9ba5
elisehellwig <echellwig@ucdavis.edu>
2017-07-02 11:00:40 -0500

    wrote the code that split up the data into primary and secondary location data frames

--- functions/tempclearn.R
+++ functions/tempclearn.R
@@ -1,4 +1,28 @@
-tempclean <- function(years, data, primary, secondary, 
-                      vars=c('')) {
+tempclean <- function(years, data, primary, secondary, hourly=FALSE) {
+    #columns should be in the order location, date, temp/tmin, (tmax)
+    
+    if (hourly) {
+        names(data) <- c('loc', 'date', 'temp')
+    } else {
+        names(data) <- c('loc', 'date','tmax', 'tmin')
+    }
+    
+    pri <- data[data$loc==primary, ]
+    
+    
+    if (length(secondary<1)) {
+        stop('You must have at least one secondary location.')
+    } else if (length(secondary)==1) {
+        sec <- data[data$loc==secondary, ]
+        
+    } else {
+        sec <- lapply(secondary, function(sloc) {
+            data[data$loc==sname, ]
+        })
+    }
+    
+    
+    
+    
     
 }
