c175e70ad6212d117afb4111f72b1b4a8fcc9be8
elisehellwig <echellwig@ucdavis.edu>
2017-07-02 11:50:56 -0500

    converted NA numbers to NAs

--- functions/tempclearn.R
+++ functions/tempclearn.R
@@ -1,11 +1,30 @@
-tempclean <- function(years, data, primary, secondary, hourly=FALSE) {
+tempclean <- function(years, data, primary, secondary, hourly=FALSE,
+                      dateform ='%Y%m%d', na_val=-9999) {
     #columns should be in the order location, date, temp/tmin, (tmax)
+    require(lubridate)
+    
     
     if (hourly) {
         names(data) <- c('loc', 'date', 'temp')
+        data[data$temp==na_val, 'temp'] <- NA
+        
     } else {
         names(data) <- c('loc', 'date','tmax', 'tmin')
+        data[data$tmin==na_val, 'tmin'] <- NA
+        data[data$tmax==na_val, 'tmax'] <- NA
+        
     }
+    
+    
+    data <- data[data$year %in% years, ]
+    
+    
+    if (!is.Date(data$date)) {
+        data$date <- as.Date(data$date, format=dateform)
+    }
+    
+    data$year <- year(data$date)
+    data$month <- month(data$date)
     
     pri <- data[data$loc==primary, ]
     
@@ -20,6 +39,8 @@ tempclean <- function(years, data, primary, secondary, hourly=FALSE) {
             data[data$loc==sname, ]
         })
     }
+    
+    
     
     
     
