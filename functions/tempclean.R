1c73c2cef5dcdb14a2c612c19b139ea58b094445
elisehellwig <echellwig@ucdavis.edu>
2017-07-05 14:41:25 -0500

    woo tempclean is doneish.

--- functions/tempclean.R
+++ functions/tempclean.R
@@ -6,6 +6,8 @@ tempclean <- function(data, primary, secondary, years=NA, hourly=FALSE,
                       dateform ='%Y%m%d', na_val=-9999) {
     #columns should be in the order location, date, temp/tmin, (tmax)
     require(lubridate)
+    require(plyr)
+    source('preTclean.R')
     #requires you to load preprocess functions before running this specifically
         #for the missingDates function
     
@@ -74,12 +76,37 @@ tempclean <- function(data, primary, secondary, years=NA, hourly=FALSE,
     
     mdat <- mm[[1]]
     for (i in 1:(length(mm)-1) ) {
+        print(i+1)
         mdat <- merge(mdat,mm[[i+1]], by='date', all=TRUE)
     }
     
-    Stmincols <- c(1, grep('Stmin', names(mdat)))
-    Stmaxcols <- c(1, grep('Stmax', names(mdat)))
+    stmin <- splitcols(mdat, 'Stmin', 'date')
+    stmin$avg <- apply(stmin[,-1], 1, mean, na.rm=TRUE)
+    
+    stmax <- splitcols(mdat, 'Stmax','date')
+    stmax$avg <- apply(stmax[,-1], 1, mean, na.rm=TRUE)
+    
+    rdata <- data.frame(loc=primary,
+                        date=as.Date(stmin[,'date'], origin='1970-01-01'),
+                        year=year(as.Date(stmin[,'date'], origin='1970-01-01')),
+                        tmin=stmin$avg,
+                        tmax=stmax$avg)
     
     
+    prir2 <- rbind(prir, rdata)
     
+    primissing2 <- missingDates(prir2)
+    
+    if (isTRUE(primissing2)) {
+        return(prir2)
+        
+    } else {
+        
+        warning('There is still missing data. More secondary locations are required to fill in the missing temperatures.')
+        
+        misslist <- list(prir2, as.Date(primissing2, origin='1970-01-01'))
+        
+        return(misslist)
+        
+    }
 }
