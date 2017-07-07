efadf32100cdcbe2a9f29d245c796d779a92b25e
elisehellwig <echellwig@ucdavis.edu>
2017-07-07 09:37:47 -0500

    starting to rewrite with all of the lovely new functions I am writing

--- preprocess/4_clim_prep.R
+++ preprocess/4_clim_prep.R
@@ -8,14 +8,42 @@ library(parallel)
 source('functions/generalfunctions.R')
 source('functions/preprocessfunctions.R')
 source('functions/tempclean.R')
+source('functions/preTclean.R')
 
 
 #This script imports climate date from CIMIS and NOAA (NCDC) and cleans it up for further processing and analysis.
 #This script focuses on min and max temperatures.
+vars <- c('id','year','date','tmin','tmax')
+yrs <- 1920:2017
+# ghcndstns <- ghcnd_stations()
+#saveRDS(ghcndstns, file.path(drivepath, 
+#                             'data/historydata/ghcndstationmetadata.RDS'))
+ghcndstns <- readRDS(file.path(drivepath, 
+                               'data/historydata/ghcndstationmetadata.RDS'))
 
+cll <- data.frame(id=c('USC00041715','USC00042294'),
+                  latitude=c(39.699513, 38.538698),
+                  longitude=c(-121.812008,-121.761336 ) )
 
+APItoken='LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN'
 #############################################################
 ##############Data Import/Prep###############################
+
+#find closest locations
+
+nearby <- meteo_nearby_stations(cll, radius=100)
+nearbychico <- as.data.frame(nearby[[1]])
+
+cs <- lapply( nearbychico$id[1:35], function(stn) {
+    ncdc_stations(stationid = paste0('GHCND:',stn), limit=50, token=APItoken)
+})
+
+#####
+##NOte: write a function that runs linear models for a whole bunch of stations 
+    #and then extracts the adjusted R squared and use that to calculate a
+    #weighted mean.
+
+
 
 ## Davis
 davstations <- c('USC00042294','USC00049742','USC00049781','USW00023232',
@@ -23,13 +51,12 @@ davstations <- c('USC00042294','USC00049742','USC00049781','USW00023232',
 
 dav <- ghncd_download(davstations)
 dateNA <- which(is.na(dav$date))
-dav2 <- dav[-dateNA, c('id','year','date','tmin','tmax')]
+dav2 <- dav[-dateNA, vars]
 
 
 Davprime <- davstations[1]
 Davsecnd<- davstations[-1]
 
-yrs <- 1920:2017
 
 davisdaily <- tempclean(data=dav2, 
                         primary=prime, 
@@ -38,11 +65,20 @@ davisdaily <- tempclean(data=dav2,
                         dateform="%Y-%m-%d")
 
 
-n <- ghcnd(stationid = stations[1])
+###Chico
 
+#chico, orland
+chicostations <- c('USC00041715', 'USC00046506')
 
+chico <- ghncd_download(chicostations)
+chicoNA <- which(is.na(chico$date))
+chico2 <- chico[-chicoNA, vars]
 
+chicoprime <- chicostations[1]
+chicosecond <- chicostations[-1]
 
+chicodaily <- tempclean(chico2, chicoprime, chicosecond, yrs, 
+                        dateform="%Y-%m-%d")
 
 #####
 #CIMIS
