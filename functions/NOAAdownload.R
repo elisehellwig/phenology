123d2e089a39488c95c0394b97407fe8a685933e
elisehellwig <echellwig@ucdavis.edu>
2017-08-25 11:56:14 -0500

    writing the upper level function to download temperature data from NCDC database

---
+++ functions/NOAAdownload.R
@@ -0,0 +1,8 @@
+ncdcDownload <- function(location, nearby=NA, limit=50, r2=0.85,
+                         token='LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN') {
+    require(rnoaa)
+    source('functions/modeltest.R')
+    
+    
+    
+}
