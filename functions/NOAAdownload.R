2fba3c4ce795022397c033bdf63673ccf9d8c306
elisehellwig <echellwig@ucdavis.edu>
2017-08-26 17:37:30 -0500

    finished a first pass at this function. We'll see if it works with the davis data.

--- functions/NOAAdownload.R
+++ functions/NOAAdownload.R
@@ -1,11 +1,15 @@
-locationDownload <- function(loc, nearby=NA, limit=50, r2=0.85, stations=40,
-                         radius=100, cores=4L,
-                         token='LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN', 
-                         variables=c('id','name','latitude','longitude',
-                                     'distance')) {
+locationDownload <- function(loc, yrs, nearby=NA, limit=50, r2=0.85, 
+                             stations=40, radius=100, cores=4L,
+                         ncdc_token='LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN', 
+                         stationvars=c('id','name','latitude',
+                                       'longitude','distance'),
+                         datavars=c('id','name','mindate','maxdate'),
+                         tempvars=c('id','year','date','tmin','tmax')) {
     require(rnoaa)
     require(parallel)
     source('functions/modeltest.R')
+    source('functions/tempclean.R')
+    
     
     if (is.na(nearby)) {
         nearbylist <- meteo_nearby_stations(location, radius=radius)
@@ -22,7 +26,7 @@ locationDownload <- function(loc, nearby=NA, limit=50, r2=0.85, stations=40,
     } else if (is.data.frame(nearby)) {
         
         nearbynames <- names(nearby)
-        missingvars <- setdiff(variables, nearbynames)
+        missingvars <- setdiff(stationvars, nearbynames)
         
         if (length(missingvars)!=0) {
             stop(paste0('You are missing the following variable(s) from your nearby station data: ', paste(missingvars, collapse=', '), '.') )
@@ -32,9 +36,32 @@ locationDownload <- function(loc, nearby=NA, limit=50, r2=0.85, stations=40,
         stop('nearby station data must be a dataframe or in the format from meteo_nearby_stations().')
     }
     
-    nearbyevals <- 
+    nearbyevals <- modeleval(nearby$id[1], nearby$id[2:stations])
+    
+    stns <- ldply( nearbyevals$id, function(stn) {
+        ncdc_stations(stationid = paste0('GHCND:',stn), limit=limit, 
+                      token=ncdc_token)$data[,datavars]
+    })
+    
+    stns$id <- sub("GHCND:", '', cs$id)
+    
+    AuxInfo <- metatemptable(stns, nearbyevals)
+    AuxInfo <- AuxInfo[AuxInfo$minR2>0.75, ]
+    AuxIDs <- AuxInfo$id
     
     
+    stnIDs <- c(loc[1,'id'], AuxIDs)
+    temp <- ghncd_download(stnIDs)
+    
+    dateNA <- which(is.na(temp$date))
+    temp2 <- temp[-dateNA, tempvars]
+    
+    prime <- stnIDs[1]
+    secnd<- stnIDs[-1]
+    
+    
+    davisdaily <- tempclean(data=temp2, primary=prime, secondary=secnd,
+                            years=yrs, dateform="%Y-%m-%d")
     
     
 }
