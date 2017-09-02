aad5ceca908106036f75342e846e80f174bb6669
elisehellwig <echellwig@ucdavis.edu>
2017-09-02 16:48:21 -0500

    script to download and format temperature data to go in the paper.

---
+++ 1_prediction/11_tempsummary.R
@@ -0,0 +1,6 @@
+drivepath <- '/Users/echellwig/Drive/Phenology'
+datapath <- file.path(drivepath, 'data/walnutdata/')
+resultspath <- file.path(drivepath,'Results/')
+library(plyr)
+library(ScrapeClim)
+library(rnoaa)
