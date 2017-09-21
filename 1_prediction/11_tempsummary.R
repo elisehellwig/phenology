71519c742c084edb2a70f04baf523e216d5da87e
elisehellwig <echellwig@ucdavis.edu>
2017-09-21 10:23:27 -0700

    formating monthly tempdata

--- 1_prediction/11_tempsummary.R
+++ 1_prediction/11_tempsummary.R
@@ -4,3 +4,68 @@ resultspath <- file.path(drivepath,'Results/')
 library(plyr)
 library(ScrapeClim)
 library(rnoaa)
+library(xts)
+library(reshape2)
+
+
+Davis <- data.frame(id=c('USC00042294'), latitude=c( 38.538698),
+                    longitude=c(-121.761336))
+
+
+temp <- readRDS(file.path(datapath, 'davisdailytemp.RDS'))
+m <- read.csv(file.path(datapath, 'monthlytemperatures.csv'))
+
+
+###########################################################################
+###########################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+###########################################################################
+###########################################################################
+tx <- xts(x = temp[,c('tmin','tmax')], order.by = temp$date)
+txmonth <- apply.monthly(tx, mean)
+tmonth <- as.data.frame(coredata(txmonth))
+tmonth$date <- as.Date(index(txmonth))
+tmonth$year <- year(tmonth$date)
+tmonth$month <- month(tmonth$date)
+
+mname <- data.frame(month=1:12, monthname=month.name)
+tmonth <- merge(tmonth, mname)
+tmm <- melt(tmonth, id.vars=c('month','date','year','monthname'))
+
+tmm$monthname <- factor(tmm$monthname,levels(tmm$monthname)[c(5,4,8,1,9,7,6,2,
+                                                              12,11,10,3)])
+
+write.csv(tmonth, file.path(drivepath, 'Results/walnutpaper/monthlytemps.csv'),
+          row.names = FALSE)
+
+tmm2 <- tmm[tmm$month %in% 3:6,]
+
+
+tplot <- ggplot(data=tmm)
+tplot <- tplot + geom_line(aes(x=year, y=value, color=variable, group=variable))
+tplot <- tplot + facet_wrap(~monthname)
+tplot <- tplot + scale_color_manual(values=c('#d7191c','#2c7bb6'), 
+                                    labels=c('Minimum','Maximum'),
+                                    name='Variable')
+tplot <- tplot +labs(x="Year", y='Temperature')
+tplot
+
+
+nearby <- meteo_nearby_stations(Davis, radius=100)
+saveRDS(nearby, file.path(drivepath, 'data/walnutpaper/nearbyDavis.RDS'))
+
+
+dav <- locationDownload(Davis, 1910:2016, nearby, r2=0.90,
+                     APItoken = 'LtpGDhfftKEwCGGgOeOsfBRCsRawIMaN')
+
