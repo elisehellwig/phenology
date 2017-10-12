b2e9b608c54d176dd9f09b80e122e85f94bf1ccb
elisehellwig <echellwig@ucdavis.edu>
2017-10-12 11:35:01 -0700

    script that calculates and analyzes trends in thermal time.

---
+++ 2_history/2_thermaltime.R
@@ -0,0 +1,9 @@
+datapath <-  '/Users/echellwig/Drive/Phenology/data/historydata'
+library(plyr)
+library(lubridate)
+library(ScrapeClim)
+library(phenoclim)
+
+davd <- readRDS(file.path(datapath, 'Davis1916.RDS'))
+
+
