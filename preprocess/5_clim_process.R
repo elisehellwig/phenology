138e5242189fca76214889f2b271e015fefa04f9
elisehellwig <echellwig@ucdavis.edu>
2017-10-16 16:05:19 -0700

    added the calculation of hourly data

--- preprocess/5_clim_process.R
+++ preprocess/5_clim_process.R
@@ -3,16 +3,40 @@ drivepath <- '/Users/echellwig/Drive/Phenology'
 library(plyr)
 library(lubridate)
 library(ScrapeClim)
+library(phenoclim)
+options(stringsAsFactors = FALSE)
 #source('functions/generalfunctions.R')
 #source('functions/preprocessfunctions.R')
 #source('functions/diurnal_temperature2.R')
+loc <- read.csv(file.path(drivepath, 'data/historydata/sitelocations.csv'))
 dav <- readRDS(file.path(drivepath, 'data/historydata/Davis1916.RDS'))
 yrs <- unique(dav$year)
+
+
 
 #temp$lat <- 38.556345
 #temp$lon <-121.815597
 
 ###############################################################
+
+##hourly temps
+davlat <- loc[loc$site=='Davis','lat']
+davhrly <- ldply(1:length(dav$date), function(i) {
+    dT <- round(diTemp(davlat, dav$date[i], dav$tmin[i], 
+                       dav$tmax[i]), 1)
+    hour <- 1:24
+    dateC <- rep(dav$date[i], 24)
+    cbind(dateC, hour, dT)
+})
+davhrly$dateC <- as.Date(davhrly$dateC, origin = '1970-01-01')
+davhrly$year <- year(davhrly$dateC)
+davhrly$month <- month(davhrly$dateC)
+davhrly$day <- yday(davhrly$dateC)
+names(davhrly)[1:3] <- c('date','hour','temp')
+saveRDS(davhrly, file=file.path(drivepath, 'data/historydata/Davis1916hr.RDS'))
+
+
+
 
 dav$month <- month(dav$date)
 dav$day <- yday(dav$date)
@@ -48,27 +72,5 @@ saveRDS(davmonth, file.path(drivepath, 'data/historydata/Davis1916month.RDS'))
 
 
 
-
-hrly <- ldply(1:length(temp$date), function(i) {
-    dT <- round(diTemp(temp$lat[i], temp$date[i], temp$tmin[i], 
-                       temp$tmax[i]), 1)
-    hour <- 1:24
-    dateC <- rep(temp$date[i], 24)
-    cbind(dateC, hour, dT)
-})
-
-hrly$dateC <- as.Date(as.character(hrly$dateC))
-hrly$hour <- as.numeric(as.character(hrly$hour))
-hrly$dT <- as.numeric(as.character(hrly$dT))
-
-hrly$year <- year(hrly$dateC)
-hrly$month <- month(hrly$dateC)
-hrly$day <- yday(hrly$dateC)
-
-names(hrly)[1:3] <- c('date','hour','temp')
-
-write.csv(hrly, 
-          file=file.path(drivepath, 'data/walnutdata/davishourlytemp.csv'), 
-          row.names = FALSE)
 
 
