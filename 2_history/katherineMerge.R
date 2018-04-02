fe8109085554e893792703b41bb61642db5cb200
elisehellwig <echellwig@ucdavis.edu>
2018-04-02 10:41:56 -0700

    getting all of the spring data that we are interested in into one file.

--- 2_history/katherineMerge.R
+++ 2_history/katherineMerge.R
@@ -1,11 +1,20 @@
 library(reshape2)
 
-datapath <- '~/Drive/Phenology/CA_Results/data'
+inpath <- '~/Drive/Phenology/CA_Results/data'
+outpath <- '~/Drive/Phenology/Results/history'
 
-almondK <- read.csv(file.path(datapath, 'Almond_CrCragHrJD.csv'),
+almondK <- read.csv(file.path(inpath, 'Almond_CrCragHrJD.csv'),
                     stringsAsFactors = FALSE)
-af <- read.csv(file.path(datapath, 'almondfloweringdata.csv'), 
+af <- read.csv(file.path(inpath, 'almondfloweringdata.csv'), 
                stringsAsFactors = FALSE)
 
 ######################################################################
 afr <- af[,c('year','cultivar','nearest','fday')] 
+names(afr)[3:4] <- c('location','bloom') 
+
+akc <- dcast(almondK, location + cultivar + year ~ requ, value.var = 'jd')
+akcr <- akc[,c('location','cultivar','year','Heat')]
+
+afnew <- merge(afr, akcr)
+
+write.csv(afnew, file.path(outpath, 'almondspring.csv'), row.names = FALSE)
