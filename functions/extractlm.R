925fc258d7560ee654308f3517cee77d0f62f481
elisehellwig <echellwig@ucdavis.edu>
2018-04-17 13:58:20 -0700

    finished the function that extracts things from linear models.

--- functions/extractlm.R
+++ functions/extractlm.R
@@ -1,5 +1,27 @@
-extractLM <- function(mod, col.names=c('coef','pval'), intname='intercept') {
+extractLM <- function(mod, col.names=c('coef','pval'), intname='intercept',
+                      loc=NA, cultivar=NA) {
+    
+    if (!grepl('summary', class(mod))) {
+        mod <- summary(mod)
+    } 
+    
     coefs <- as.data.frame(coef(mod))
     names(coefs) <- c('coef','stderr','tval','pval')
-    coefs$var <- row.names(coefs)
+   
+    varnames <- c(intname, row.names(coefs)[-1])
+    coefs$var <- varnames 
+    row.names(coefs) <- NULL
+    
+    coefs$r2 <- mod$adj.r.squared
+    
+    if (!is.na(loc)) {
+        coefs$location <- loc
+    }
+    
+    if (!is.na(cultivar)) {
+        coefs$cultivar <- cultivar
+    }
+    
+    return(coefs)
+    
 }
