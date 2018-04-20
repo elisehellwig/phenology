118eb0e8b93eb65e1be4559b1c53bfafb937617e
elisehellwig <echellwig@ucdavis.edu>
2018-04-20 11:17:06 -0700

    added a script to bring in precip data

---
+++ preprocess/6_precipprocess.R
@@ -0,0 +1,11 @@
+library(reshape2)
+
+chico <- read.csv(file.path(data3, 'chicoprecip.csv'))
+modes  <- 
+parlier
+davis
+
+
+####################################################################
+
+
