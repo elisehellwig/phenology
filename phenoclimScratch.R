32cc2cd549ba9b05176913090a03f4f966d8c87e
elisehellwig <echellwig@ucdavis.edu>
2018-11-26 10:36:37 -0800

    testing script

--- phenoclimScratch.R
+++ phenoclimScratch.R
@@ -1,6 +1,6 @@
 
 # Setup -------------------------------------------------------------------
-
+library(reshape2)
 library(lubridate)
 library(phenoclim)
 library(DEoptim)
@@ -8,34 +8,29 @@ library(DEoptim)
 
 options(stringsAsFactors = FALSE)
 
-datapath <- '/Users/echellwig/Drive/Phenology/data/walnutdata'
+datapath <- '/Users/echellwig/Drive/Phenology/data/flowering'
 funpath <- '/Users/echellwig/Research/phenoclim/R'
 
-dav <- read.csv(file.path(datapath, 'davisdailyhourlytemp2.csv'))
+dav <- read.csv(file.path(datapath, 'davisdailyhourlytemp.csv'))
 wall <- read.csv(file.path(datapath, 'walnutclean.csv'))
 
-#la <- paste(dav$date, 'FlowerModel', paste0(dav$hour, ':00:00'))
-#write.csv(dav, file.path(datapath, 'davisdailyhourlytemp2.csv'),
- #   row.names = FALSE)
-#dav$dt <- dayToDate(dav$year, dav$day, 'FlowerModel', dav$hour-1)
+dav$dt <- as.POSIXct(dav$dt, format="%Y-%m-%d %H:%M:%OS")
 
-dav$dt <- as.POSIXct(dav$dt)
-
-subyrs <- 1981:1992
-
-ww <- which(wall$year %in% c(subyrs) & wall$cultivar=='Payne' )
+ww <- which( wall$cultivar=='Chandler')
 w <- wall[ww,]
 
-wf <- data.frame(year=w$year[-1],
-                 event1=w$event1[-1],
-                 event0=w$event2[-nrow(w)])
+wc <- dcast(w, year + cultivar~ event, value.var = 'day')
 
+wf <- data.frame(year=wc$year,
+                 event1=wc$event1,
+                 event0=300)
+wf <- wf[complete.cases(wf),]
 
 
 # Troubleshooting thermal sums --------------------------------------------
 
 
- #flowerSums <- thermalsum(list(0), wf$year, dav, 'TTT', 'gdd', 5,                       2000, varying=c('start'), 'FlowerModel', wf$event0)
+ flowerSums <- thermalsum(list(NA), wf$year, dav, 'TTT', 'utah', 50,                       300, varying=NA, 'FlowerModel', wf$event0)
 #
 #
 # # troubleshooting minrmse -------------------------------------------------
@@ -46,31 +41,33 @@ wf <- data.frame(year=w$year[-1],
 #                    varying = 'start', 'FlowerModel', wf$event0)
 #
 #
-# error2 <- minrmseTTT(6, wf, dav, 'linear', 6, 10, 1,
-#                    varying = NA, 'FlowerModel', wf$event0)
+ error2 <- minrmseTTT(7.2, wf, dav, 'chillbasic', 275, 20, 1,
+                    varying = NA, 'FlowerModel')
 #
 #
 # error3 <- minrmseTTTsimplified(0, wf, dav, 'linear', 20, 2000, 1,
 #                      varying = NA, 'FlowerModel', wf$event0)
 #
- #errorTT <- minrmse(c(1, 12, 0), wf, dav, 'DT', 'gdd', 1, TRUE, TRUE,
-                 #  TRUE, FALSE, NA, 'FlowerModel', 'event0')
+
+errorTT <- minrmse(c(50, 300), wf, dav, 'TTT', 'chillbasic', 1, 7.2, 
+                   50, 300, TRUE, NA, 'FlowerModel', 'event0')
 #
 #
 # # troubleshooting flowermodel -----------------------------------------------
 
-forms <- c('gdd', 'linear')
+forms <- c('chillbasic', 'linear','gdd','anderson')
+CTs <- list(7.2, 4, 4, c(4,25,36))
 
-parlist <- lapply(forms, function(f) {
+parlist <- lapply(seq_along(forms), function(i) {
     parameterlist(n=1,
-                  mt='DT',
+                  mt='TTT',
                   simple=FALSE,
-                  ff=f,
-                  ct=list(0),
-                  modelthreshold=12,
-                  start=5,
-                  varyingparameters=c('start','threshold'),
-                  optimized=c('cardinaltemps'),
+                  ff=forms[i],
+                  ct=CTs[i],
+                  modelthreshold=100,
+                  start=275,
+                  varyingparameters=NA,
+                  optimized=c('threshold','start'),
                   ModelClass='FlowerModel')
 })
 
@@ -82,13 +79,23 @@ parlist <- lapply(forms, function(f) {
 # cores <- 4
 #
 
-fm <- flowermodel(phenology=wf,
+fm2 <- flowermodel(phenology=wf,
                   temps=dav,
                   parlist=parlist,
-                  lbounds=c(1),
-                  ubounds=c(50),
+                  lbounds=c(1,1),
+                  ubounds=c(365,2000),
                   cores = 4L)
 
 
-fmc <- crossval(fm, dav, 3, 2029239, 'rmse', 1, 50, 50, 4L)
 
+chandler <- readRDS('/Volumes/GoogleDrive/My Drive/Phenology/Results/flowering/TTTChandler.RDS')
+
+fmc <- crossval(object=chandler,
+                temps=dav, 
+                k=3, 
+                seed=2029239, 
+                fun='rmse', 
+                lbounds=c(1,1), 
+                ubounds=c(365, 1000), 
+                iterations=50,
+                cores=4L)
