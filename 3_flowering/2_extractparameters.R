a95f2b511c8d6c0690258ce345d1e354cea318f3
elisehellwig <echellwig@ucdavis.edu>
2018-11-28 14:48:15 -0800

    am adding code to extract results into a data frame.

--- 3_flowering/2_extractparameters.R
+++ 3_flowering/2_extractparameters.R
@@ -2,6 +2,23 @@ library(lubridate)
 library(phenoclim)
 library(DEoptim)
 
-datapath <- '/Volumes/GoogleDrive/My Drive/Phenology/Results/flowering'
+rpath <- '/Volumes/GoogleDrive/My Drive/Phenology/Results/flowering'
+dpath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/flowering'
 
-payne <- readRDS(file.path(datapath, 'TTTPaynetest.RDS'))
+w <- read.csv(file.path(dpath, 'walnutclean.csv'), stringsAsFactors = FALSE)
+
+cults <- sort(unique(w$cultivar))
+
+simplefiles <- list.files(path=rpath,
+                          pattern='CVsimple.RDS',
+                          full.names=TRUE)
+
+walfiles <- list.files(path=rpath,
+                       pattern='CV.RDS',
+                       full.names=TRUE)
+
+
+simpleMods <- lapply(simplefiles, function(fn) readRDS(fn))
+walMods <-  lapply(walfiles, function(fn) readRDS(fn))
+
+la <- extractResults(simplefiles, cults, 'chillbasic')
