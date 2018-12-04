55ff6e5f3216083f9ba29656384664ce478d0884
elisehellwig <echellwig@ucdavis.edu>
2018-12-04 12:20:39 -0800

    preliminary code for extracting values from the flowermodels fit on farm

--- 3_flowering/2_extractparameters.R
+++ 3_flowering/2_extractparameters.R
@@ -1,11 +1,19 @@
 library(lubridate)
 library(phenoclim)
 library(DEoptim)
+source('functions/helperfunctions.R')
 
 rpath <- '/Volumes/GoogleDrive/My Drive/Phenology/Results/flowering'
 dpath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/flowering'
 
 w <- read.csv(file.path(dpath, 'walnutclean.csv'), stringsAsFactors = FALSE)
+dav <- read.csv(file.path(dpath, 'davisdailyhourlytemp.csv'))
+dav$dt <- as.POSIXct(dav$dt, format="%Y-%m-%d %H:%M:%OS")
+
+wc <- w[w$cultivar=='Chandler',]
+wc1 <- dcast(wc, year + cultivar~ event, value.var = 'day')
+wc2 <- wc1[complete.cases(wc1), ]
+wc2$event0 <- 300
 
 cults <- sort(unique(w$cultivar))
 
@@ -17,8 +25,13 @@ walfiles <- list.files(path=rpath,
                        pattern='CV.RDS',
                        full.names=TRUE)
 
-
 simpleMods <- lapply(simplefiles, function(fn) readRDS(fn))
 walMods <-  lapply(walfiles, function(fn) readRDS(fn))
 
-la <- extractResults(simplefiles, cults, 'chillbasic')
+names(simpleMods) <- cults
+names(walMods) <- cults
+#la <- extractResults(simplefiles, cults, 'chillbasic')
+
+sm <- summarizeModel(simpleMods, cults)
+wm <- summarizeModel(walMods, cults)
+
