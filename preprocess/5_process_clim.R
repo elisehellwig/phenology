ad8fd980224ec5f59317fbf8a087d36a6168b78f
elisehellwig <echellwig@ucdavis.edu>
2018-12-04 12:18:49 -0800

    updated the script to interpolate hourly data using interpol.T instead of diTemp

--- preprocess/5_process_clim.R
+++ preprocess/5_process_clim.R
@@ -2,7 +2,9 @@ datapath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/'
 library(lubridate)	
 library(phenoclim)
 library(plyr)
+library(Interpol.T)
 
+source('functions/datetime.R')
 #source('R/functions/generalfunctions.R')
 source('functions/diurnal_temperature2.R')
 
@@ -58,52 +60,24 @@ davhourly <- Th_int_series(cal_times=cpar,
 
 dh <- davhourly$Date
 dh$temp <- davhourly$Davis
-write.csv(dh, file.path(datapath, 'clean/interpolatedDavis.csv'),
-          row.names = FALSE)
+#write.csv(dh, file.path(datapath, 'clean/interpolatedDavis.csv'),
+         # row.names = FALSE)
 
-
-dtstring <- paste0( dh$year, '-', dh$month, '-', dh$day, ' ', 
-                    sprintf("%02d", dh$hour), ':00:00')
-dh$dt <- as.POSIXct(dtstring, format="%Y-%m-%d %H:%M:%OS")
-
+dhdt <- convertToDT(dh, c('dt','temp'))
+dhdt$date <- POSIXtoDate(dhdt$dt)
 #locs <- c('Chico','Davis', 'Manteca','Parlier')
-#####################Create Hourly Temperatures####################
 
 
-#davis <- temp[temp$nearest=='Davis',]
-#davis$date <- as.Date(davis$date)
+# Merge with daily temperatures -------------------------------------------
 
-hrdav <- ldply(1:length(davis$date), function(i) {
-    temp <- round(diTemp(38.544476, davis[i, 'date'], davis[i,'tmin'], 
-                         davis[i, 'tmax']), 1)
-    hour <- 1:24
-    date <- rep(as.Date(davis$date[i]), 24)
-    data.frame(date, hour, temp)
-})
-
-hrdav$day <- yday(hrdav$date)
-hrdav$year <- year(hrdav$date)
-
-dt <- merge(hrdav, davis)
-dt$hourstring <-  ifelse(nchar(dt$hour-1)<2, paste0(0, dt$hour-1), 
-                         as.character(dt$hour-1))
-dt$dt <- as.POSIXct(paste(dt$date, 
-                          paste0(dt$hourstring, ':00:00')),
-                    format="%Y-%m-%d %H:%M:%OS")
-
-missingDTs <- which(is.na(dt$dt))
-dtMiss <- dt[missingDTs, ]
-dtstring <- paste0(dtMiss$date, ' ', dtMiss$hourstring, ':00:00')
-dt$dt[missingDTs] <- dtstring
+dt <- merge(dhdt, davis, by='date')
+dtfinal <- dt[,c('dt','year','day','temp','tmin','tmax')]
+dtfinal <- dtfinal[order(dtfinal$dt), ]
 
 
-mindt <- min(dt$dt)
-maxdt <- max(dt$dt)
-dtfinal <- dt[,c('dt','year','day','hour','temp','tmin','tmax')]
+cim$date <- toPOSIX(cim$date)
 
-cim$date <- as.POSIXct(cim$date, format="%Y-%m-%d %H:%M:%OS")
- 
-tdfinal$temp <- sapply(1:nrow(dtfinal), function(i) {
+dtfinal$temp <- sapply(1:nrow(dtfinal), function(i) {
     dtdate <- dtfinal[i, 'dt']
     
     if (dtdate %in% cim$date) {
@@ -119,18 +93,6 @@ tdfinal$temp <- sapply(1:nrow(dtfinal), function(i) {
     }
 })
 
-write.csv(dtfinal, file.path(datapath, 'davisdailyhourlytemp.csv'), 
+write.csv(dtfinal, file.path(datapath, 'clean/davisdailyhourlytemp.csv'), 
           row.names = FALSE)
-######################################
 
-testdf <- temp[temp$nearest=='Chico', ]
-
-la <- diTemp(39.693182, testdf$date, testdf$tmin, testdf$tmax)
-
-htemp$date <- as.Date(htemp$date)
-htemp$day <- yday(htemp$date)
-htemp$month <- month(htemp$date)
-htemp$year <- year(htemp$date)
-
-
-write.csv(htemp, file='data/clean/hourlytemp.csv')
