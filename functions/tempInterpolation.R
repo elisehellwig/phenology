fe97cf59d39eb35aeee205edb7afa95cd2aea76d
elisehellwig <echellwig@ucdavis.edu>
2019-01-17 10:12:22 -0800

    added a function to automate the merging of the daily and hourly temp datasets.

--- functions/tempInterpolation.R
+++ functions/tempInterpolation.R
@@ -20,9 +20,11 @@ InterpTemp <- function(daily, hourly, name, start, end) {
     
     cal <- calibrateInterp(hourly, name)
     
+    print(1)
     cpar <- cal[[1]]
     cshape <- cal[[2]]
     
+    print(2)
     dmin <- data.frame(year = year(daily$date),
                        month = month(daily$date),
                        day = day(daily$date),
@@ -39,21 +41,56 @@ InterpTemp <- function(daily, hourly, name, start, end) {
     
     hourlyInterp<- Th_int_series(cal_times=cpar, 
                                  cal_shape = cshape,
-                                 TMIN=dtmin, 
-                                 TMAX=dtmax, 
+                                 TMIN=dmin, 
+                                 TMAX=dmax, 
                                  start_year=start, 
                                  end_year=end,
                                  active_IDs = name)
-    
+    print(4)
     
     tempTS <- hourlyInterp$Date
-    tempTS$temp <- hourlyInterp$Davis
-    #write.csv(dh, file.path(datapath, 'clean/interpolatedDavis.csv'),
-    # row.names = FALSE)
+    tempTS$temp <- hourlyInterp[[name]]
     
+    print(5)
     tempdt <- convertToDT(tempTS, c('dt','temp'))
     tempdt$date <- POSIXtoDate(tempdt$dt)
     
     return(tempdt)
+}
+
+
+mergeDailyHourly <- function(daily, hourly, interpolated) {
+    
+    daily$date <- as.Date(daily$date)
+    
+    dt <- merge(interpolated, daily, by='date')
+    dtfinal <- dt[,c('dt','year','day','temp','tmin','tmax')]
+    dtfinal$hour <- hour(dtfinal$dt)
+    dtfinal <- dtfinal[order(dtfinal$dt), ]
+    
+    
+    hourly$date <- toPOSIX(hourly$date)
+    
+    #print(length(which(is.na(dtfinal$dt))))
+    dtfinal$temp <- sapply(1:nrow(dtfinal), function(i) {
+        dtdate <- dtfinal[i, 'dt']
+        
+        #print(dtdate)
+        #print(head(hourly$date))
+        if (dtdate %in% hourly$date) {
+            if (!is.na(hourly[which(hourly$date==dtdate),'temp'])) {
+                hourly[which(hourly$date==dtdate),'temp']
+            } else {
+                dtfinal[i, 'temp']
+            }
+            
+        } else {
+            dtfinal[i, 'temp']
+            
+        }
+    })
+    
+    return(dtfinal)
+    
 }
 
