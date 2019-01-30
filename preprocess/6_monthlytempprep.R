c2585d995bbdb219079518412b68a9be0b6e5741
elisehellwig <echellwig@ucdavis.edu>
2019-01-30 14:56:10 -0800

    this script calculates monthly and annual min max and avg temperatures. It also calculates a moving average for the annual temps.

--- preprocess/6_monthlytempprep.R
+++ preprocess/6_monthlytempprep.R
@@ -3,6 +3,77 @@ datapath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/'
 library(lubridate)	
 library(phenoclim)
 library(plyr)
+library(dplyr)
+library(raster)
+source('functions/cleanTemps.R')
 
 
 dht <- readRDS(file.path(datapath, 'clean/dailyhourlytemp.RDS'))
+
+
+# Calculating Monthly Temps -----------------------------------------------
+
+tminMonth <- AverageTemps(dht, 'tmin')
+tmaxMonth <- AverageTemps(dht, 'tmax')
+
+monthly <- merge(tminMonth, tmaxMonth, by=c('string','year','month','loc'))
+
+monthly$tavg <- (monthly$tmin + monthly$tmax)/2
+
+monthly$tmin <- round(monthly$tmin, 1)
+monthly$tmax <- round(monthly$tmax, 1)
+monthly$tavg <- round(monthly$tavg, 1)
+
+monthly$loc <- recode(monthly$loc, 'modest'='modesto', 'parlie'='parlier')
+monthly$nmonth <- month.name[monthly$month]
+
+monthly <- monthly[,c('loc','year','month','nmonth','tmin','tmax','tavg')]
+
+
+# Calculating Annual Temps ------------------------------------------------
+
+tminAnnual <- AverageTemps(dht, 'tmin', monthly=FALSE)
+tmaxAnnual <- AverageTemps(dht, 'tmax', monthly=FALSE)
+
+annual <- merge(tminAnnual, tmaxAnnual, by=c('string','year','loc'))
+
+annual$tavg <- (annual$tmin + annual$tmax)/2
+
+annual$tmin <- round(annual$tmin, 1)
+annual$tmax <- round(annual$tmax, 1)
+annual$tavg <- round(annual$tavg, 1)
+
+annual$loc <- recode(annual$loc, 'modest'='modesto', 'parlie'='parlier')
+
+annual <- annual[,c('loc','year','tmin','tmax','tavg')]
+
+
+# Calculate 5 year moving average -----------------------------------------
+
+
+oldnames<-c('tmin', 'tmax','tavg')
+newnames <-c('Minimum', 'Maximum','Average') #moving averages
+
+
+for (i in unique(annual$loc)) {
+    for (j in 1:3) {
+        s <- which(annual$loc==i)
+        annual[s,newnames[j]] <- round(movingFun(annual[s, oldnames[j]], 
+                                           5, na.rm=TRUE),1) 
+        
+        
+    }  
+}
+
+
+
+# Saving data -------------------------------------------------------------
+
+write.csv(annual, file.path(datapath, 'clean/annualtemperatures.csv'), 
+          row.names = FALSE)
+
+write.csv(monthly, file.path(datapath, 'clean/monthlytemperatures.csv'),
+          row.names = FALSE)
+
+
+
