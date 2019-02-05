f0f1ae9bd7177a38d42af7302573103c53ef9410
elisehellwig <echellwig@ucdavis.edu>
2019-02-05 09:55:10 -0800

    this will be used to calculate thermal time with new models.

---
+++ 2_history/1_thermalTime.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+library(lubridate)
+
+options(stringsAsFactors = FALSE)
+
+historypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/history'
+phenologypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/phenology'
+
