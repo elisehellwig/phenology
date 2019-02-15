f9784603a196072d12d3b834eba74935d8546b02
elisehellwig <echellwig@ucdavis.edu>
2019-02-15 15:51:53 -0800

    added a function that calculates thermal time for a 'long' phenology dataset

--- functions/thermaltimesupport.R
+++ functions/thermaltimesupport.R
@@ -1,5 +1,6 @@
 extendphenology <- function(fdat, firstyear, lastyear=NA) {
-    
+    #fills in phenological events with the average day for years where it is 
+    #missing not yet observed.
     fdat <- fdat[,c('year','event1','event2','length1')]
     
     avgevent1 <- round(mean(fdat[,'event1']))
@@ -21,3 +22,66 @@ extendphenology <- function(fdat, firstyear, lastyear=NA) {
     
     return(fdatextended)
 }
+
+
+calcThermalTime <- function(events, temperatures, modtype, form, 
+                            cardinal, start, thresh, varying, 
+                            location=NA, var=NA, datename='dt') {
+    source('functions/datetime.R')
+    #this is just for harvest at this point
+    #note loc must be the name of the column that determines location
+    
+    if (!is.POSIXct(temperatures[,datename])) {
+        temperatures[,datename] <- toPOSIX(temperatures[,datename])
+    }
+    
+    
+    if (!is.na(location)) {
+        events <- filter(events, loc==location)
+        temperatures <- filter(temperatures, loc==location)
+    }
+    
+    if (!is.na(var)) {
+        events <- filter(events, cultivar==var)
+    }
+    
+    events <- unique(events)
+    
+    yrtable <- table(events[,'year'])
+    
+    if (any(ifelse(yrtable>1, TRUE, FALSE))) {
+        
+        eventsC <- dcast(events, cultivar + loc+year ~ event, value.var = 'day')
+        eventsC <- eventsC[complete.cases(eventsC), ]
+        
+    } else {
+        eventsC <- events
+    } 
+    
+    
+    
+    years <- eventsC[,'year']
+    startdays <- eventsC[,'event1']
+    
+    startdates <- dayToDate(years, startdays, 'PlantModel', varying)
+    
+    if (modtype=='DT') {
+        threshvalues <- startdates + days(thresh)
+    } else {
+        threshvalues <- thresh
+    }
+    
+    
+    sums <- thermalsum(cardinal, years, temperatures, modtype, form,
+                       startdates, threshvalues, varying, 'PlantModel',
+                       startdays)
+    
+    
+  
+    eventsC[,paste0(modtype, form)] <- sums
+    
+    return(eventsC)
+    
+}
+
+
