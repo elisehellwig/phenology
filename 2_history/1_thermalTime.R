76b2c568d66035d56a1827f225a09c5f05a8636c
elisehellwig <echellwig@ucdavis.edu>
2019-02-15 15:52:11 -0800

    calculating thermal time for almonds walnuts and prunes for harvest

--- 2_history/1_thermalTime.R
+++ 2_history/1_thermalTime.R
@@ -1,8 +1,52 @@
+
+# Setup -------------------------------------------------------------------
+
 library(tidyverse)
 library(lubridate)
+library(reshape2)
+library(plyr)
+library(phenoclim)
 
 options(stringsAsFactors = FALSE)
+source('functions/datetime.R')
+source('functions/thermaltimesupport.R')
 
 historypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/history'
 phenologypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/phenology'
+
+a <- unique(read.csv(file.path(phenologypath,'almondclean.csv')))
+p <- unique(read.csv(file.path(phenologypath,'pruneclean.csv')))
+w <- read.csv(file.path(phenologypath,'walnutclean.csv'))
+
+temp <- readRDS(file.path(phenologypath, 'dailyhourlytemp.RDS'))
+temp$dt <- as.POSIXct(temp$dt, format="%Y-%m-%d %H:%M:%OS")
+
+
+# Almond Harvest ----------------------------------------------------------
+asl <- a %>% 
+    filter(source=='RAVT', loc %in% c('Chico','Modesto'),
+                    cultivar %in% c('Nonpareil','Mission'))
+
+LocVar <- expand.grid(unique(asl$loc), unique(asl$cultivar))
+names(LocVar) <- c('loc','cultivar')
+
+ah <- ldply(1:nrow(LocVar), function(i) {
+    calcThermalTime(asl, temp, 'DT', 'asymcur', c(4,25,36), 0, 60, 
+                    c('start','threshold'), location = LocVar[i,'loc'],
+                    var=LocVar[i,'cultivar'])
+
+})
+
+ah$length1 <- ah$event2 - ah$event1
+
+
+# Prune harvest -----------------------------------------------------------
+
+psl <- calcThermalTime(p, temp, 'DT', 'asymcur', c(4,25,36), 0, 60, 
+                    c('start','threshold'), location = 'Parlier',
+                    var='French')
+
+
+psl$length1 <- psl$event2 - psl$event1
+
 
