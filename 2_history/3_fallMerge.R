2d48f4ad15e5c0f8f33a0825942ffd61aee5aac5
elisehellwig <echellwig@ucdavis.edu>
2019-03-11 09:17:56 -0700

    renamed a script to make it be in the right order

--- 2_history/3_fallMerge.R
+++ 2_history/3_fallMerge.R
@@ -1,37 +1,89 @@
+
+# This script 
+
+# Setup -------------------------------------------------------------------
+
 library(tidyverse)
 library(lubridate)
+library(reshape2)
+library(plyr)
+library(phenoclim)
 
-path <- '/Volumes/GoogleDrive/My Drive/Phenology/Results/history'
+options(stringsAsFactors = FALSE)
+source('functions/datetime.R')
+source('functions/thermaltimesupport.R')
 
-almond <- read_csv(file.path(path, 'almondseasonlengthdata.csv'))
-prune <- read_csv(file.path(path, 'pruneseasonlengthdata.csv'))
-walnut <- read_csv(file.path(path, 'walnutseasonlengthdata.csv'))
+historypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/history'
+phenologypath <- '/Volumes/GoogleDrive/My Drive/Phenology/data/phenology'
 
-thermal <- read_csv(file.path(path, 'thermaltimeaccumulation.csv'))
+a <- unique(read.csv(file.path(phenologypath,'almondclean.csv')))
+p <- unique(read.csv(file.path(phenologypath,'pruneclean.csv')))
+w <- read.csv(file.path(phenologypath,'walnutclean.csv'))
+precip <- read.csv(file.path(historypath, 'precipitation.csv'))
 
-####################################################################
-####################################################################
-
-a <- select(almond, -contains("_")) %>% 
-    add_column(crop='almond')
-
-p <- select(prune, -contains("_")) %>% 
-    add_column(crop='prune', cultivar='French')
-
-
-w <- select(walnut, -contains("_")) %>% 
-    add_column(crop='walnut')
-
-th <- thermal %>% 
-    select(-simplified, heatsum=anderson)
-
-####################################################################
-####################################################################
-
-fall <- bind_rows(a, p, w) %>%
-    inner_join(th) %>%
-    mutate(heatscl=ave(heatsum, cultivar, FUN=scale))
+amod <- readRDS(file.path(historypath, 'SLalmondDTanderson.RDS'))
+pmod <- readRDS(file.path(historypath, 'SLpruneDTanderson.RDS'))
 
 
-write_csv(fall, file.path(path, 'fall.csv'))
+temp <- readRDS(file.path(phenologypath, 'dailyhourlytemp.RDS'))
+temp$dt <- as.POSIXct(temp$dt, format="%Y-%m-%d %H:%M:%OS")
 
+
+# Extract model thresholds ------------------------------------------------
+locVar <- expand.grid(c('Chico','Modesto'), c('Mission','Nonpareil'))
+names(locVar) <- c('loc','cultivar')
+
+locVar$threshold <- sapply(amod, function(m) round(unlist(threshold(m))))
+
+pruneThreshold <- round(unlist(threshold(pmod)))
+
+# Almond Harvest ----------------------------------------------------------
+ah <- a %>% 
+    filter(source=='RAVT', loc %in% c('Chico','Modesto'),
+                    cultivar %in% c('Nonpareil','Mission'))
+
+asl <- ldply(1:nrow(locVar), function(i) {
+    calcThermalTime(ah, temp, 'harvest', 'DT', 'asymcur', c(4,25,36), 0, 
+                    locVar[i, 'threshold'], c('start','threshold'), 
+                    location = locVar[i,'loc'], var=locVar[i,'cultivar'])
+
+})
+
+asl$length1 <- asl$event2 - asl$event1
+asl$crop <- 'almond'
+
+# Prune harvest -----------------------------------------------------------
+
+psl <- calcThermalTime(p, temp, 'harvest', 'DT', 'asymcur', c(4,25,36), 0, 
+                       pruneThreshold, c('start','threshold'), 
+                       location = 'Parlier', var='French')
+
+
+psl$length1 <- psl$event2 - psl$event1
+
+psl$crop <- 'prune'
+
+# Walnut harvest ----------------------------------------------------------
+
+cv <- c('Chandler','Payne','Franquette')
+w$loc <- 'Davis'
+mt <- c('TTT', 'DT', 'DT')
+frm <- c('flat','gdd','asymcur')
+thresh <- c(50127.33,62,46)
+ct <- list(c(0.4, 12.1), 11.1, c(4,25,36))
+
+wsl <- ldply(seq_along(cv), function(i) {
+    calcThermalTime(w, temp, 'harvest', mt[i], frm[i], ct[[i]], 0, thresh[i], 
+                       c('start','threshold'), var=cv[i])
+    })
+
+wsl$length1 <- wsl$event2 - wsl$event1
+wsl$crop <- 'walnut'
+
+# Combine harvest ---------------------------------------------------------
+
+harvest <- rbind(asl, psl, wsl)
+harvestprecip <- merge(harvest, precip, by.x=c('loc','year'), 
+                       by.y=c('location','year'))
+
+saveRDS(harvestprecip, file.path(historypath, 'harvest.RDS'))
