cb82cb1958d4d58a9d8269d511b6d53bc5a3ef1a
elisehellwig <echellwig@ucdavis.edu>
2019-03-12 14:42:53 -0700

    added prune and walnut thermal time calculations

--- 2_history/4_thermaltimeseries.R
+++ 2_history/4_thermaltimeseries.R
@@ -42,3 +42,43 @@ asl <- ldply(1:nrow(aLocVar), function(i) {
     
 })
 
+
+# Prune -------------------------------------------------------------------
+
+p <- spring %>% 
+    filter(crop=='prune',event=='event1') %>% 
+    select(loc, cultivar, year, 'event1'=day)
+
+psl <- ldply(unique(p$loc), function(l) {
+    calcThermalTime(p, temp, 'harvest', 'DT', 'asymcur', c(4,25,36), 0, 
+                    locVar[5, 'threshold'], c('start','threshold'), 
+                    location = l, predictor='thermal')
+})
+
+
+# Walnut ------------------------------------------------------------------
+
+wLocVar <- filter(locVar, crop=='walnut')
+
+w <- spring %>% 
+    filter(crop=='walnut',event=='event1') %>% 
+    select(loc, cultivar, year, 'event1'=day)
+
+ct <- list(c(0.4, 12.1), 11.1, c(4,25,36))
+
+wsl <- ldply(1:nrow(wLocVar), function(i) {
+    calcThermalTime(w, temp, 'harvest', wLocVar[i, 'modtype'], 
+                    wLocVar[i,'form'], ct[[i]], 0, wLocVar[i, 'threshold'], 
+                    c('start','threshold'), var=wLocVar[i,'cultivar'],
+                    predictorName='thermal')
+})
+
+
+
+# Combine data and save ---------------------------------------------------
+
+sl <- rbind(asl, psl, wsl)
+
+saveRDS(sl, file.path(historypath, 'ThermalTimeSeries.RDS'))
+
+
