bfc1646bbf427d61fbe31dfa599116fdd571971c
elisehellwig <echellwig@ucdavis.edu>
2019-03-29 15:44:39 -0700

    calculated non-cv RMSE errors

--- 2_history/6_crossvalidation.R
+++ 2_history/6_crossvalidation.R
@@ -17,3 +17,23 @@ harv <- readRDS(file.path(historypath, 'harvestfit.RDS'))
 tts <- readRDS(file.path(historypath, 'ThermalTimeSeries.RDS'))
 locVar <- read.csv(file.path(historypath, 'SeasonLengthParameters.csv'))
 
+
+# Errors ------------------------------------------------------------------
+
+datalist <- lapply(1:nrow(locVar), function(i) {
+    filter(harv, crop==locVar[i,'crop'], loc==locVar[i, 'loc'], 
+           cultivar==locVar[i, 'cultivar'])
+})
+
+locVar$RMSE <- sapply(1:nrow(locVar), function(i) {
+    round(rmse(datalist[[i]]$fitlength, datalist[[i]]$length1),2)
+})
+
+locVar$RMSE30 <- sapply(1:nrow(locVar), function(i) {
+    round(rmse(datalist[[i]]$fitlength30, datalist[[i]]$length1),2)
+})
+
+
+
+
+
