7aebbec6e7dff0e97a80410bf66df56862f63aa8
elisehellwig <echellwig@ucdavis.edu>
2019-03-29 16:04:23 -0700

    function that calculates crossvalidated rmse error.

---
+++ functions/cvfunction.R
@@ -0,0 +1,37 @@
+
+
+CV <- function(df, predictor, response, k=5, seed=NA, avg=TRUE) {
+    
+    if (!is.na(seed)) {
+        set.seed(seed)
+    }
+    
+    df$fold <- kfold(df, k)
+    
+    error <- rep(NA, k)
+    
+    
+    for (i in 1:k) {
+        train <- df[df$k!=i, ]
+        test <- df[df$k==i, ]
+         
+        fmla <- paste(response, '~', predictor)
+        
+        mod <- lm(as.formula(fmla), data=train)
+        
+        newdata <-data.frame(test[,predictor])
+        names(newdata) <- predictor
+        
+        fit <- predict(mod, newdata)
+        
+        error[i] <- rmse(fit, test[,response])
+        
+    }
+    
+    if(avg) {
+        error <- mean(error)
+    }
+    
+    return(error)
+    
+}
