b3a2e0de4397ad0a6bde1a8ec580ee9f3f72da33
elisehellwig <echellwig@ucdavis.edu>
2019-05-16 11:09:18 -0700

    commented these to functions

--- functions/extractlm.R
+++ functions/extractlm.R
@@ -1,24 +1,38 @@
 extractLM <- function(mod, col.names=c('coef','pval'), intname='intercept',
                       loc=NA, cultivar=NA) {
+    #This function extracts various parameters from an 'lm' object
+    # mod - 'lm' or 'summary.lm' object, from which information is to be
+        #extracted
+    # col.names - character, vector of the parameter categories to be extracted
+    # intname - character, what to call the intercept coefficient
+    # loc - character, name of the location the data was from
+    # cultivar - character, name of the cultivar the data represents
+    # OUTPUT - a dataframe containing coefficients, standard errors, tvalues,
+        #pvalues, adjusted r2 and location and cultivar labels if available
     
+    #convert mod to 'summary.lm' object if it is not already one
     if (!grepl('summary', class(mod))) {
         mod <- summary(mod)
     } 
     
+    #create data.frame of the coefficients of the model
     coefs <- as.data.frame(coef(mod))
+    
+    #give data frame appropriate names
     names(coefs) <- c('coef','stderr','tval','pval')
    
+    #rename the intercept so it doesn't have parentheses in the name
     varnames <- c(intname, row.names(coefs)[-1])
-    coefs$var <- varnames 
-    row.names(coefs) <- NULL
+    coefs$var <- varnames  #give coefficient names their own column
+    row.names(coefs) <- NULL #remove row names
     
-    coefs$r2 <- mod$adj.r.squared
+    coefs$r2 <- mod$adj.r.squared #add adjusted r2 to data frame
     
-    if (!is.na(loc)) {
+    if (!is.na(loc)) { #add location if availiable
         coefs$loc <- loc
     }
     
-    if (!is.na(cultivar)) {
+    if (!is.na(cultivar)) { #add cultivar if available
         coefs$cultivar <- cultivar
     }
     
@@ -28,7 +42,18 @@ extractLM <- function(mod, col.names=c('coef','pval'), intname='intercept',
 
 formatLM <- function(df, lmlist, cols=c('coef', 'pval','r2'), crop=NA,
                      dropIntercept=TRUE) {
+    #df - data.frame, contains location ('loc') and cultivar labels for each
+        #model in the lmlist
+    #lmlist - list, a list of 'lm' or 'summary.lm' objects the data will be
+        #extracted from
+    #cols - character, the variables to be extracted from the lm object.
+        #Options: 'coef', 'stderr', 'tval', 'pval', and 'r2'
+    #crop - character, the crop the data represents
+    #dropIntercept logical, should the intercept be dropped from the
+        #coefficients returned
     
+    
+    #extract the information from each lm object
     if (class(lmlist)=='lm') {
         moddf <- extractLM(lmlist, loc=df[1,'loc'], cultivar=df[1,'cultivar'])
         
@@ -39,23 +64,26 @@ formatLM <- function(df, lmlist, cols=c('coef', 'pval','r2'), crop=NA,
         
     }
     
+    #remove intercept if it is not needed
     if (dropIntercept) {
         moddf <- filter(moddf, var!='intercept')
     } else {
         cols <- c('var', cols)
     }
    
-    
+    #create columns of interest
     coi <- c('loc','cultivar', cols)
     
+    #select only columns of interest
     newdf <- moddf[,coi]
     
+    #add a column identifying crop if it is provided
     if (!is.na(crop)) {
         newdf[,'crop'] <- crop
     }
     
     
-    
+    #return lm information data frame.
     return(newdf)
 }
 
