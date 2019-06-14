ec147b709af6c784471aad84a610c2e3576c672f
elisehellwig <echellwig@ucdavis.edu>
2019-06-14 11:18:54 -0700

    updated some stuff for the almond model

--- 2_history/1_optimize.R
+++ 2_history/1_optimize.R
@@ -26,9 +26,12 @@ wcults <- c('Chandler','Payne','Franquette')
 wc <- dcast(w, cultivar+year ~ event, value.var='day')
 wcc <- wc[complete.cases(wc),]
 
+wcc$loc <- 'Davis'
+
 walnutData <- lapply(1:length(wcults), function(i) {
     filter(wcc, cultivar==wcults[i])
 })
+
 
 walnutDT <- list(parameterlist(1, 'DT', FALSE, 'asymcur', list(c(4,25,36)), 30, 
                               0, c('start','threshold'), 'threshold', 
@@ -37,7 +40,8 @@ walnutDT <- list(parameterlist(1, 'DT', FALSE, 'asymcur', list(c(4,25,36)), 30,
 
 wpmDT <- lapply(seq_along(walnutData), function(i) {
     print(i)
-    plantmodel(walnutData[[i]], temp, walnutDT, 0, 270, cores=4L)
+    plantmodel(walnutData[[i]], temp, walnutDT, 0, 270, cores=4L, 
+               iterations=25)
 })
 
 saveRDS(wpmDT, file.path(historypath, 'SLwalnutDTanderson.RDS'))
@@ -61,7 +65,9 @@ saveRDS(ppmDT, file.path(historypath, 'SLpruneDTanderson.RDS'))
 
 asub <- a %>% 
     filter(cultivar %in% c('Nonpareil','Mission','Sonora'),
-           source=='RAVT')
+           source=='RAVT', loc != 'Shafter') 
+
+asub$loc <- recode(asub$loc, "Modesto"="Manteca")
 
 asub <- unique(asub)
 
@@ -73,7 +79,7 @@ acc <- ac[complete.cases(ac),]
 vars <- c('Mission','Nonpareil','Sonora')
 
 almondData <- lapply(vars, function(cv) {
-    select(filter(acc, cultivar==cv), c(cultivar, year, event1, event2))
+    filter(acc, cultivar==cv)
 })
 
 almondDT <- lapply(c('asymcur'), function(form) {
@@ -92,11 +98,8 @@ almondDT <- lapply(c('asymcur'), function(form) {
 apmDT <- lapply(seq_along(almondData), function(i) {
     print(i)
     plantmodel(almondData[[i]], temp, almondDT, 0, 270, cores=4L,
-               iterations=25)
+               iterations=200)
 })
 
 
 saveRDS(apmDT, file.path(historypath, 'SLalmondDTanderson.RDS'))
-
-
-
